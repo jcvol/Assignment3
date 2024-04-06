eecd31d61b3c64f17b67f4500b7e9f1593a4395b
Julia Volheim <143303084+jcvol@users.noreply.github.com>
2024-04-05 23:59:34 -0400

    Update Rover.java

--- src/Rover.java
+++ src/Rover.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.concurrent.*;
 
 public class Rover {
@@ -12,9 +13,12 @@ public class Rover {
 
         var reporter = new Runnable(){
             public void run(){
+                final var readingsList = new ArrayList<>(readings);
+                readingsList.sort((a,b)->Integer.compare(a.temperature, b.temperature));
                 System.out.println("Highest temperatures: ");
-                
+                System.out.println();
                 System.out.println("Lowest temperatures: ");
+
             }
         };
 
